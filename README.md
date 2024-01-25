b7dbaefbc0367be5f25272715582dab75eeb49e4
Piyush Garg <22243371+piyush888@users.noreply.github.com>
2024-01-24 17:38:20 -0700

    Building login workflow tester
    - Working for site without ReCAPTCHA
    - Resolving ReCAPTCHA renders the webdriver unable to find other elements on page
    - Updated README for execution instructions

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Website-Daily-Test
+# Selenium Website Tester
+
+This script is designed for testing website interactions, specifically for automated login workflows. It utilizes the Selenium framework to control web browsers, allowing you to specify a preferred web driver and perform testing on different websites.
+
+## Requirements
+
+- Python 3.x
+- Selenium library
+
+## Usage
+
+### Arguments
+
+- `--web-driver`: Specify your preferred web driver (default: firefox).
+- `--website`: Input the target website URL (default: www.google.com).
+- `--login-email`: Input your login email or username.
+- `--login-password`: Input your login password.
+- `--recaptcha-enabled`: Include this flag if the login involves ReCAPTCHA.
+
+### Example
+
+```bash
+python script.py --web-driver chrome --website https://example.com --login-email user@example.com --login-password password --recaptcha-enabled
---
+++ selenium_tester.py
@@ -0,0 +1,92 @@
+import time
+import argparse
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from urllib3.exceptions import NewConnectionError, MaxRetryError
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class WebsiteTester():
+    def __init__(self, web_driver: str = "firefox"):
+        self.driver = self.get_webdriver(web_driver=web_driver)
+
+    def get_webdriver(self, web_driver):
+        if web_driver=="firefox": return webdriver.Firefox()
+        elif web_driver=="chrome": return webdriver.Chrome()
+        elif web_driver=="safari": return webdriver.Safari()
+        elif web_driver=="iexplorer": return webdriver.Ie()
+        elif web_driver=="webkitgtk": return webdriver.WebKitGTK()
+        elif web_driver=="edge": return webdriver.Edge()
+        elif web_driver=="wpewebkit": return webdriver.WPEWebKit()
+        else:
+            return webdriver.Chrome()
+
+
+    def sel_test_website(self, website="https://aidronesoftware.com/", *args, **kwargs):
+        self.driver.get(website)
+        
+        self.login_workflow(login_email=kwargs['login_email'], login_password=kwargs['login_password'], recaptcha=kwargs['recaptcha'])
+
+        self.driver.close()
+
+
+    def login_workflow(self, login_email, login_password, recaptcha=False):
+        # Await the loading of a certain element on the webpage - in this case the login button
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+                        (By.XPATH, "/html/body/div/nav/div/div[2]/a[2]")))
+        
+        element.click()
+
+        # Await the loading of Email and Password Field
+        email_elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+                        (By.XPATH, "/html/body/div[1]/div/div/div[1]/div/form/div[1]/input")))
+        email_elem.send_keys(login_email)
+        password_elem = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(
+                        (By.XPATH, "/html/body/div[1]/div/div/div[1]/div/form/div[2]/input")))
+        password_elem.send_keys(login_password)
+
+        print("Trying to find Login button...")
+        login = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@class='w-100 btn btn-primary' and @type='submit' and contains(text(), 'Log In')]")))
+        # login = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'btn-primary') and contains(text(), 'Log In')]")))
+
+        # Modify for your ReCAPTCHA element
+        if recaptcha:
+            print("ATTEMPTING CAPTCHA...")
+            captcha_iframe = WebDriverWait(self.driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '/html/body/div[1]/div/div/div[1]/div/form/div[3]/div/div/div/div/iframe')))
+            captcha = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.recaptcha-checkbox-border")))
+            captcha.click()
+        
+        # Click on login button and wait
+        # if login.is_enabled():
+        #     login.click()
+        # else:
+        #     login.click()
+        #     login.click()
+        time.sleep(2)
+        print("Attempting login click...")
+        login.click()
+
+
+if __name__ == "__main__":
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    
+    # Adding optional argument
+    parser.add_argument("--web-driver", help = "Input your preferred web-driver", choices=['firefox', 'chrome', 'safari', 'iexplorer', 'webkitgtk', 'edge', 'wpewebkit'], default='firefox')
+    parser.add_argument("--website", help = "Input your website", default='www.google.com')
+    parser.add_argument("--login-email", help = "Input your login email or username")
+    parser.add_argument("--login-password", help = "Input your login password")
+    parser.add_argument('--recaptcha-enabled', action='store_true', help='Login is ReCAPTCHA enabled')
+
+    
+    args = parser.parse_args()
+    
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    tester = WebsiteTester(web_driver=args.web_driver)
+    tester.sel_test_website(website=args.website, login_email=args.login_email, login_password=args.login_password, recaptcha=args.recaptcha_enabled)
